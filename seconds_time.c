9148b94385ef1caff438abdfc894b2ee18db1628
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 19:57:13 +0800

    Update seconds_time.c

--- seconds_time.c
+++ seconds_time.c
@@ -5,29 +5,24 @@ int main()
     int Sec = 0;
     int Min = 0;
     int Hour = 0;
-    char name[80]="";
+    char name[80] = "";
 
     printf("Enter your name: ");
     scanf("%s", name);
     printf("Enter seconds: ");
     scanf("%d", &Sec);
 
-    Hour = Sec/3600;
-    Sec %= 3600; // need %=
-    Min = Sec/60;
-    Sec %+60;
-    
+    Hour = Sec / 3600;          // Calculate hours
+    Sec %= 3600;                // Get remaining seconds after hours calculation
+    Min = Sec / 60;             // Calculate minutes
+    Sec %= 60;                  // Get remaining seconds after minutes calculation
 
+    // Display the converted time
     printf("Seconds: %d\n", Sec);
     printf("Minutes: %d\n", Min);
     printf("Hours: %d\n", Hour);
 
-    printf("%s, the time is %.2d:%.2d:%.2d", name, Hour, Min, Sec);
+    printf("%s, the time is %.2d:%.2d:%.2d\n", name, Hour, Min, Sec);
 
-
-
-    
-
-
-
+    return 0;
 }
