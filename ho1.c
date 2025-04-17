ab706bdc11a1be72e9161e3d2fe252d88b6a088e
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 20:44:46 +0800

    Update ho1.c

--- ho1.c
+++ ho1.c
@@ -30,36 +30,20 @@ return x;
 }
 
 void diagonalSearch(int array[3][3], int dv){
-    int i, j;
-    for( i=0;i<3;i++){
-    	for(j=0;j<3;j++){
-            if (array[i][j]== dv){
-            	if (i==j){
-            		printf("Element found at Row: %d, Column: %d.\n",i, i);
-				} else {
-					printf("Element not found on the diagonal.\n");
-				}
-        	} 
-        }
-    }        
-    printf("Element does not exist.\n");
-}
-
-void altdiagSearch(int array[3][3], int key){
     int i,j=0;
     
     for (i=0;i<3;i++){
-        if (array[i][i] == key){
-            printf("Value %d found at row %d column %d.\n", key, i,i);
+        if (array[i][i] == dv){
+            printf("Value %d found at row %d column %d.\n", dv, i,i);
             j++;
             }
         }
     if (j==0){
         printf("num not in diagonal.\n");
     }      
-}     
+}           
 
-void diagsecSearch(int array[3][3], int key){
+void diagSecSearch(int array[3][3], int key){
     int i,j=0;
     int row =3;
     
@@ -168,49 +152,86 @@ void cornerSum(int array[3][3]){
 }
 
 int main(){
- int arr[3][3];
- int again, again1;
- int i,j;
- int foundInArray = 0, foundInDiagonal = 0;
- int key;
- 
- printf("Welcome to my program!\n");
- 
- do{
-    for( i=0;i<3;i++){
-         for (j=0;j<3;j++){
-             printf("Enter value for Row: %d Column: %d : ", i,j);
-             arr[i][j] = valueCheck();
-         }
-    }
-             
-    for( i=0;i<3;i++){
-         for (j=0;j<3;j++){
-             printf("%d\t", arr[i][j]);
-         }
-         printf("\n");
-    }
-    
-    printf("Enter value to be searched: ");
-    key = valueCheck();
-    
-    diagsecSearch(arr, key);             
-                 
-    printf("Would you like to try again? 1-Yes/0-No: ");
-    again = againCheck();
-    if (again == 0){
-        printf("Do you want to exit the program? 1-Yes/0-No: ");
+    int arr[3][3];
+    int again, again1;
+    int i, j;
+    int key;
+    int choice;
+
+    printf("Welcome to my program!\n");
+
+    do {
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("Enter value for Row: %d Column: %d : ", i, j);
+                arr[i][j] = valueCheck();
+            }
+        }
+
+        printf("\nARRAY:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("%d\t", arr[i][j]);
+            }
+            printf("\n");
+        }
+
+        do {
+            printf("\nWhat would you like to do?\n");
+            printf("1. Search value in primary diagonal\n");
+            printf("2. Search value in secondary diagonal\n");
+            printf("3. Find maximum value\n");
+            printf("4. Show all values divisible by 3\n");
+            printf("5. Show all even values\n");
+            printf("6. Sum of border values\n");
+            printf("7. Sum of corner values\n");
+            printf("Enter your choice (1-7): ");
+            choice = valueCheck();
+
+            switch (choice) {
+                case 1:
+                    printf("Enter value to be searched in primary diagonal: ");
+                    key = valueCheck();
+                    diagonalSearch(arr, key);
+                    break;
+                case 2:
+                    printf("Enter value to be searched in secondary diagonal: ");
+                    key = valueCheck();
+                    diagSecSearch(arr, key);
+                    break;
+                case 3:
+                    maximumSearch(arr);
+                    break;
+                case 4:
+                    div3Search(arr);
+                    break;
+                case 5:
+                    evenSearch(arr);
+                    break;
+                case 6:
+                    borderSum(arr);
+                    break;
+                case 7:
+                    cornerSum(arr);
+                    break;
+                default:
+                    printf("Invalid choice. Please enter a number between 1 and 7.\n");
+                    break;
+            }
+
+            printf("\nWould you like to perform another operation on the same array? 1-Yes/0-No: ");
+            again = againCheck();
+
+        } while (again == 1);
+
+        printf("Do you want to enter a new array? 1-Yes/0-No: ");
         again1 = againCheck();
-        if (again1 == 1){
+        if (again1 == 0) {
             printf("Thank you for using my program!\n");
             break;
-        } 
-		else {
-        	again = 1;
         }
-    }
-    
-} while (again ==1);
- 
- return 0;   
+
+    } while (1);
+
+    return 0;
 }
