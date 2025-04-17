15a650a7fb702f8924198aadcdf5b3c899697cc9
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 20:00:54 +0800

    Update wages.c

--- wages.c
+++ wages.c
@@ -14,21 +14,22 @@ int main ()
     printf("How many hours have you worked in a week: ");
     scanf("%d", &hours);
 
-    
-
     if (hours >= 0) {
         if (hours <= 40) {
+            // Regular wage for up to 40 hours
             rWage = hours * 10;
-            printf("%s, your hourly wage is %d", name, rWage);
-        } else if (hours >= 41) {
-            rWage = hours * 10;
+            printf("%s, your weekly wage for %d hours is %d.\n", name, hours, rWage);
+        } else {
+            // Regular wage for the first 40 hours
+            rWage = 40 * 10;
+            // Overtime calculation for hours beyond 40
             oTime = hours - 40;
-            tWage = rWage + (oTime * 1.5);
-            printf("%s, you're an overtimer! Your wage is %d", name, tWage);
+            tWage = rWage + (oTime * 15);  // Overtime wage at 1.5x the regular rate
+            printf("%s, you're an overtimer! Your total wage for %d hours is %d.\n", name, hours, tWage);
         }
     } else {
-        printf("Invalid Input! Please try again.");
+        printf("Invalid Input! Please try again.\n");
     }
 
     return 0;
-}   
+}
