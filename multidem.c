507c8632b947ec4fa968b3e4fd0902a9c2278ceb
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 20:15:32 +0800

    Update multidem.c

--- multidem.c
+++ multidem.c
@@ -1,18 +1,18 @@
 #include <stdio.h> 
 
 int main() {
-    int r =2;
+    int r = 2;
     int c = 3;
     int X[r][c];
     int found = 0;
 
     printf("Please Input 6 Values.\n");
-    
-    //Get inputs for the 2d array
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            printf("X[%d][%d]: ", i,j);
-            if(scanf("%d", &X[i][j]) != 1) {
+
+    // Get inputs for the 2d array
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("X[%d][%d]: ", i, j);
+            if (scanf("%d", &X[i][j]) != 1) {
                 printf("Invalid Input!\n");
                 return 1;
             }
@@ -25,18 +25,19 @@ int main() {
         printf("Invalid Input\n");
         return 1;
     }
-    
-    for (int i = 0; i < c; i++){
-        for(int j = 0; j < r; j++){
-            if(X[i][j] == key){
-                printf("Existing Value!\nIndex: X[%d][%d]\n",i,j);
+
+    for (int i = 0; i < r; i++) { // loop through rows
+        for (int j = 0; j < c; j++) { // loop through columns
+            if (X[i][j] == key) {
+                printf("Existing Value!\nIndex: X[%d][%d]\n", i, j);
                 found = 1;
                 break;
-            }   
+            }
         }
+        if (found) break; // Break outer loop if value is found
     }
 
-    if (found == 0){
+    if (found == 0) {
         printf("Value Not Found!\n");
     }
 
