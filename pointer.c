7c064c6d2b72a12ae3414fe25a2363a2ae6ae436
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 20:52:00 +0800

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 	int x = 10;
-	int *ptr = &x;// referencing to address of x
-	int **j = &ptr; // referencing to address of ptr
-	
+	int *ptr = &x;   // ptr stores the address of x (single pointer)
+	int **j = &ptr;  // j stores the address of ptr (double pointer)
+
+	// Display address and value of x
 	printf("Address of x: %x\n", &x);
-	printf("Value of x: %d\n", x); // value from x
+	printf("Value of x: %d\n", x); // prints the value stored in x
 	
-	*ptr= 13;
+	// Modify x using the single pointer
+	*ptr = 13;
 	printf("\n");
-	printf("New value of x: %d\n", x);
-	printf("Address of ptr: %x\n", &ptr); //address ni ptr
-	printf("Address of x through ptr: %x\n", ptr); // address pointer to x
-	printf("Value of x through ptr: %d\n", *ptr); // address pointer to x
-	
+	printf("New value of x: %d\n", x); // x is updated to 13 through ptr
+	printf("Address of ptr: %x\n", &ptr); // address where ptr is stored
+	printf("Address of x through ptr: %x\n", ptr); // ptr points to x
+	printf("Value of x through ptr: %d\n", *ptr); // dereferencing ptr gives value of x
+
+	// Modify x again using the double pointer
 	**j = 2;
 	printf("\n");
-	printf("New value of x: %d\n", x);
-	printf("Address of j: %x\n", &j); //address ni j
-	printf("Address of x through ptr pointed from j: %x\n", *j); // address pointer to ptr
-	printf("Value of x through ptr pointed from j: %d\n", **j); // value pointer to ptr
-	
+	printf("New value of x: %d\n", x); // x is updated to 2 through double pointer
+	printf("Address of j: %x\n", &j); // address where j (double pointer) is stored
+	printf("Address of x through ptr pointed by j: %x\n", *j); // *j is ptr, which points to x
+	printf("Value of x through ptr pointed by j: %d\n", **j); // **j dereferences to x
+
 	return 0;
 }
