90319f6e11f3fe94b5432ff4a9201fba5909c2d2
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-17 19:58:09 +0800

    Update smiley.c

--- smiley.c
+++ smiley.c
@@ -9,8 +9,11 @@ int main ()
     int Smiley = 0;
     char name[80] = "";
 
+    // Ask for user's name
     printf("Please enter your name: ");
     scanf("%s", name);
+
+    // Ask for three numbers
     printf("Please enter the 1st number: ");
     scanf("%d", &n1);
     printf("Please enter the 2nd number: ");
@@ -18,9 +21,12 @@ int main ()
     printf("Please enter the 3rd number: ");
     scanf("%d", &n3);
 
+    // Calculate the sum and then double it
     Sum = n1 + n2 + n3;
     Smiley = Sum * 2;
 
-    printf("Twice the sum of %s's numbers %d, %d, %d is %d :) :) :)", name, n1, n2, n3, Smiley);
+    // Print the result with a smiley face
+    printf("Twice the sum of %s's numbers %d, %d, and %d is %d :) :) :)\n", name, n1, n2, n3, Smiley);
 
+    return 0;
 }
