0aace977ce26fb82664738f36e2cb25c3c473b4d
Jayvie Lorenz Enorme <jayviegillaco527@gmail.com>
2025-04-23 22:57:21 +0800

    Update error_handlers.c

--- error_handlers.c
+++ error_handlers.c
@@ -1,62 +1,63 @@
 #include <stdio.h>
 
-#define EMPLOYEE_COUNT 2
-
-// Struct for storing time
-struct Time {
-	int hour;
-	int minute;
-};
-
-// Struct for storing employee records
-struct EmployeeRecord {
-	char name[80];
-	struct Time timeIn;
-	struct Time timeOut;
-};
-
-// Input validation for integers > 0
-int inputCheck(const char *prompt){
+int inputCheck(){
 	int x;
 	char c;
 	while(1){
-		printf("%s", prompt);
-		if(scanf("%d", &x)==1 && x >= 0){
-			while((c=getchar())!='\n' && c!=EOF); // clear buffer
+		printf("\nEnter an integer: ");
+		if(scanf("%d", &x)==1 && x>0){
+			while((c=getchar())!='\n' && c!= EOF);
 			return x;
 		}
-		printf("Error! Please enter a valid positive integer.\n");
-		while((c=getchar())!='\n' && c!=EOF); // clear buffer
+		printf("Error! Enter a valid integer.\n");
+		while((c=getchar())!='\n' && c!= EOF);
 	}
 }
 
-int main(){	
-	int i;
-	struct EmployeeRecord employees[EMPLOYEE_COUNT];
-
-	for ( i = 0; i < EMPLOYEE_COUNT; i++) {
-		printf("\n---- Enter Details for Employee %d: \n", i + 1);
-
-		// Requests Name
-		printf("Enter employee name: ");
-		fgets(employees[i].name, sizeof(employees[i].name), stdin);
-
-		// Requests Time Check-in & Check-out
-		employees[i].timeIn.hour = inputCheck("Enter Time In (Hour 0-23): ");
-		employees[i].timeIn.minute = inputCheck("Enter Time In (Minute 0-59): ");
-
-		employees[i].timeOut.hour = inputCheck("Enter Time Out (Hour 0-23): ");
-		employees[i].timeOut.minute = inputCheck("Enter Time Out (Minute 0-59): ");
+int valueCheck(){
+	int x;
+	char c;
+	while(1){
+		printf("\nEnter an integer: ");
+		if(scanf("%d", &x)==1){
+			while((c=getchar())!='\n' && c!= EOF);
+			return x;
+		}
+		printf("Error! Enter a valid integer.\n");
+		while((c=getchar())!='\n' && c!= EOF);
 	}
+}
 
-	// Print all records
-	printf("\n===== Employee Records =====\n");
-	for ( i = 0; i < EMPLOYEE_COUNT; i++) {
-		printf("\nEmployee %d:\n", i + 1);
-		printf("Name     : %s", employees[i].name);
-		printf("Time In  : %02d:%02d\n", employees[i].timeIn.hour, employees[i].timeIn.minute);
-		printf("Time Out : %02d:%02d\n", employees[i].timeOut.hour, employees[i].timeOut.minute);
+int againCheck(){
+	int x;
+	char c;
+	while(1){
+		printf("\nError! Try again? 1-Yes / 0-No: ");
+		if(scanf("%d", &x)==1 && (x==0||x==1)){
+			while((c=getchar())!='\n' && c!= EOF);
+			return x;
+		}
+		printf("Error! Enter 1 for Yes or 0 for No.\n");
+		while((c=getchar())!='\n' && c!= EOF);
 	}
+}
 
+
+int main(){
+	int num, value, retry;
+	
+	printf("Testing Input Check: ");
+	num = inputCheck();
+	printf("You entered %d\n\n", num);
+	
+	printf("Testing value Check: ");
+	value = valueCheck();
+	printf("You entered %d\n\n", value);	
+
+	printf("Testing again Check: ");
+	retry = againCheck();
+	printf("You entered %d\n\n", retry);	
+	
+	
 	return 0;
 }
